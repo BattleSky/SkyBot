dac032484c0de134bc76c9d1adbc4f8a34fa75f8
Stepan Filippov <28298843+BattleSky@users.noreply.github.com>
2021-01-06 23:43:26 +0300

    Добавьте файлы проекта.

---
+++ WoWCheck/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace WoWCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainTask(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainTask(string[] args)
+        {
+            var discordConnect = new DiscordConnect();
+            var discord = discordConnect.CreateClient();
+
+            discord.MessageCreated += async e =>
+            {
+                var message = e.Message.Content;
+                if (message.StartsWith("&"))
+                {
+                    await e.Message.RespondAsync("Hello, " +e.Author.Username);
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
