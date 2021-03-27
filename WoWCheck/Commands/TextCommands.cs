39a97d6e0d9f7b409fae2c8ad25c16716a5b3fb2
Stepan Filippov <28298843+BattleSky@users.noreply.github.com>
2021-03-27 14:09:13 +0300

    formatting

--- WoWCheck/Commands/TextCommands.cs
+++ WoWCheck/Commands/TextCommands.cs
@@ -5,7 +5,7 @@ using DSharpPlus.CommandsNext.Attributes;
 
 namespace WoWCheck.Commands
 {
-    class TextCommands : BaseCommandModule
+    internal class TextCommands : BaseCommandModule
     {
         [Command("hi")]
         [Description("Поприветствуй бота!")]
@@ -14,43 +14,5 @@ namespace WoWCheck.Commands
             var author = ctx.User.Mention;
             await ctx.RespondAsync("Привет, " + author);
         }
-        //[Command("affix")]
-        //[Description("Показать расписание модификаторов эпохальных подземелий")]
-        //public async Task AffixCommand(CommandContext ctx, string all)
-        //{
-        //    var affixesModule = new ();
-        //    var embed = affixesModule.AffixRequest(ctx.User.AvatarUrl);
-        //    await ctx.RespondAsync(embed: embed.Result);
-        //}
-
-
-
-
-        #region Фигня всякая
-
-        // Не работает? 
-        // Не получает список имен в ctx.Channel.Users
-        //[Command("hi")]
-        //public async Task GreetCommandMentionsOther(CommandContext ctx, string name)
-        //{
-        //    if (ctx.Channel.Id != 241874656318062593) return;
-        //    var mention = "";
-        //    foreach (var user in ctx.Channel.Users)
-        //    {
-        //        Console.WriteLine("user:" + user.DisplayName + "\n");
-
-        //        if (user.DisplayName != name || user.Nickname != name) continue;
-        //        mention = user.Mention;
-        //        break;
-        //    }
-
-        //    if (mention == "")
-        //        await ctx.RespondAsync("Не понял, кому передать привет");
-        //    else
-        //        await ctx.RespondAsync("Тебе привет, " + mention);
-        //}
-
-        #endregion
-
     }
 }
